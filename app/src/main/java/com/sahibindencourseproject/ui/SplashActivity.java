80b435d755f6e9b8ad9f4575741f6ed41383d907
Ataer Caner Celik <caner.celik@sahibinden.com>
2019-12-02 14:35:19 +0300

    no message

--- app/src/main/java/com/sahibindencourseproject/ui/SplashActivity.java
+++ app/src/main/java/com/sahibindencourseproject/ui/SplashActivity.java
@@ -21,6 +21,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
         }, 750);
     }
